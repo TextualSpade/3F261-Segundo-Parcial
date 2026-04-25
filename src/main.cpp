282d816ad142360058aaa452728d077e7ff4abb3
TextualSpade <a24310295@ceti.mx>
2026-04-25 03:08:36 +0000

    Practica 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <SerieFocos.hpp>
+#include <Foco.hpp>
+
 int main(int argc, char const *argv[])
 {
-    
-    return EXIT_SUCCESS;
+    SerieFocos serie;
+    serie.EncenderFocos();
+    serie.MostrarFocos();
+    serie.ApagarFocos();
+    serie.MostrarFocos();
+
+    return 0;
 }
 )

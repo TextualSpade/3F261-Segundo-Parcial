13c02a4187756acc1c062fb178a3862ab560cfed
TextualSpade <a24310295@ceti.mx>
2026-04-29 01:55:46 +0000

    Practica 4

---
+++ src/memoria.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int hola;
+    cout << &hola << endl;
+    bool ejemplo;
+    cout << &ejemplo << endl;
+    return 0;
+}
